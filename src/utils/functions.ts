308e64a07a8ad93180ca6fa3ff85acd939115d99
Hritique Rungta <hritiquerungta@gmail.com>
2021-12-29 12:52:23 +0530

    higher order func to batch async requests

--- src/utils/functions.ts
+++ src/utils/functions.ts
@@ -21,6 +21,12 @@ export const downloadFile = async (data: string, fileName: string) => {
   aTag.remove();
 };
 
+export const wait = async (time: number) => {
+  return new Promise((resolve) => {
+    setTimeout(resolve, time);
+  });
+};
+
 export const addDecimalNumberStrings = (
   strings: string[],
   decimals: number = 9
@@ -28,4 +34,27 @@ export const addDecimalNumberStrings = (
   return strings.reduce((prev, curr) => {
     return Number((prev + Number(curr)).toFixed(decimals));
   }, 0);
+};
+
+export const batchRequests = async <T, Y>(
+  iterator: Array<T>,
+  batchVolume: number,
+  waitTime: number,
+  asyncFunction: (arg: T, index: number) => Promise<Y>
+) => {
+  let responses: Array<Y | undefined> = [];
+  for (let i = 0; i < iterator.length; i += batchVolume) {
+    responses.push(
+      ...(
+        await Promise.allSettled(
+          iterator.slice(i, i + batchVolume).map((a) => asyncFunction(a, i))
+        )
+      ).map((p) => {
+        return p.status === 'fulfilled' ? p.value : undefined;
+      })
+    );
+    await wait(waitTime);
+  }
+
+  return responses;
 };
